a928b2afd48c812c6a63595fee9ebff71c3f8b5d
NguyenDuyPhong1204 <nguyenduyphong12122004@gmail.com>
2024-04-09 08:38:55 +0700

    commit

---
+++ Lab_5/app/src/main/java/com/example/lab_5_ph36760/Update_SinhVien.java
@@ -0,0 +1,19 @@
+package com.example.lab_5_ph36760;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Update_SinhVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_sinh_vien);
+
+    }
+}
